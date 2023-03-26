a3abd297abd0dd135e8f6c90b86f841e5142ee14
Glaicon <gjpeixer@gmail.com>
2023-03-25 21:54:14 -0300

    update main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { Polybase } from "@polybase/client";
+import { useAuth, usePolybase } from "@polybase/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { auth, state, loading } = useAuth();
+  const polybase = usePolybase();
+
+  async function create_record() {
+    await polybase.collection("User").create(["new-york3", "New-york2"]);
+  }
 
   return (
     <div className="App">
+      <div>
+        <button onClick={() => auth.signIn()}>Sign In</button>
+        <button onClick={() => auth.signOut()}>Sign Out</button>
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +31,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => create_record()}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { PolybaseProvider, AuthProvider } from "@polybase/react";
+import { Polybase } from "@polybase/client";
+import { Auth } from "@polybase/auth";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const polybase = new Polybase({
+  defaultNamespace: import.meta.env.VITE_REACT_NAMESPACE,
+  signer: async (data) => {
+    return {
+      h: "eth-personal-sign",
+      sig: await auth.ethPersonalSign(data),
+    };
+  },
+});
+const auth = new Auth();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <PolybaseProvider polybase={polybase}>
+      <AuthProvider auth={auth} polybase={polybase}>
+        <App />
+      </AuthProvider>
+    </PolybaseProvider>
+  </React.StrictMode>
+);
